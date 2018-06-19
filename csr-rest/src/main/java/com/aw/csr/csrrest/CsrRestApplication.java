65b916960b0218304263d57f5ac05de7fa529551
abeym <abey_mail@yahoo.com>
2018-06-18 23:26:29 -0400

    hsql db writing to the script file in csr-rest\db\sql

--- csr-rest/src/main/java/com/aw/csr/csrrest/CsrRestApplication.java
+++ csr-rest/src/main/java/com/aw/csr/csrrest/CsrRestApplication.java
@@ -1,12 +1,32 @@
 package com.aw.csr.csrrest;
 
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+//@EnableTransactionManagement
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
+//    DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+
 public class CsrRestApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CsrRestApplication.class, args);
   }
+
+  @Bean
+  @ConfigurationProperties("spring.datasource")
+  public DataSource dataSource() {
+    return DataSourceBuilder.create().build();
+  }
+
 }
